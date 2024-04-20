e05a24b873d9f9df3cd36e1165909dad35425d70
wiredquill <erin@suse.com>
2024-04-20 07:14:35 -0700

    Update youtube-scrape.py

--- youtube-scrape.py
+++ youtube-scrape.py
@@ -35,15 +35,23 @@ def get_channel_videos(api_key, channel_id):
 
     return videos
 
-def print_video_info(videos):
+def search_videos(videos, query):
+    matching_videos = []
     for video in videos:
         title = video['snippet']['title']
         description = video['snippet']['description']
 
-        print(f'Title: {title}')
-        print(f'Description: {description}')
-        print('---')
+        if query.lower() in title.lower() or query.lower() in description.lower():
+            matching_videos.append(title)
+
+    return matching_videos, len(matching_videos)
 
 # Example usage
 videos = get_channel_videos(API_KEY, CHANNEL_ID)
-print_video_info(videos)
+query = "susecon"
+matching_titles, total_matching_videos = search_videos(videos, query)
+
+print(f'Total number of videos matching the criteria: {total_matching_videos}')
+print('Matching titles:')
+for title in matching_titles:
+    print(title)
